62fa8a52b270b9cdfe6fae9e2b7272dbd7970d0a
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-19 23:49:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(array)
   new_array = []
   new_array << array.split
   new_array.each do |word|
-    word.reverse_each
+    new_array << word.reverse_each
   end
   new_array
 end
